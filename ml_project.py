a29992dcf50f5af5d211e1907a7552aeafe376ea
Gaëtan <59415807+gaetanbrison@users.noreply.github.com>
2024-11-25 14:19:22 +0100

    Update ml_project.py

--- ml_project.py
+++ ml_project.py
@@ -755,6 +755,21 @@ if app_mode == 'Deployment 🚀':
 
 ## page 5 bis 
 if app_mode == 'MLFlow2 ☁️':
+    df = pd.read_csv("wine_quality_red.csv")
+    from sklearn.model_selection import train_test_split
+
+    # Load the dataset
+    df = pd.read_csv("wine_quality_red.csv")
+    
+    # Define features (X) and target variable (y)
+    # Assuming "quality" is the target column
+    X = df.drop(columns=["quality"])
+    y = df["quality"]
+    
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     import mlflow
     from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
